639d50b34eed78d89a540790063f00f19b7bde92
Pedro Kons <pedro_kons@edu.univali.com.br>
2025-06-30 00:50:34 -0300

    exclused route hello test

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,6 @@
 import { FastifyInstance } from "fastify";
-import hello from "./hello";
 import user from "./user";
 
 export default async function routes(fastify: FastifyInstance) {
-  await fastify.register(hello, { prefix: "/hello" });
   await fastify.register(user, { prefix: "/user" });
 }
